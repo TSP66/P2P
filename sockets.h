3eba8fa9b8b914106f28872a25f0fe74678c7f43
TSP66 <62830753+TSP66@users.noreply.github.com>
2020-04-08 12:20:53 +1000

    Add files via upload

---
+++ sockets.h
@@ -0,0 +1,119 @@
+#include <unistd.h> 
+#include <stdio.h> 
+#include <sys/socket.h> 
+#include <stdlib.h> 
+#include <netinet/in.h> 
+#include <string.h> 
+#include <arpa/inet.h> 
+
+#define PORT 8089
+
+char * message;
+
+char buffer[1024] = {0}; 
+
+int sock = 0;
+ 
+int setup_as_server() 
+{ 
+	int server_fd, valread; 
+	struct sockaddr_in address; 
+	int opt = 1; 
+	int addrlen = sizeof(address); 
+	
+	
+	
+	// Creating socket file descriptor 
+	if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) 
+	{ 
+		perror("socket failed"); 
+		exit(EXIT_FAILURE); 
+	} 
+	
+	// Forcefully attaching socket to the port 8080 
+	/*if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, 
+												&opt, sizeof(opt))) 
+	{ 
+		perror("setsockopt"); 
+		exit(EXIT_FAILURE); 
+	}
+*/ 
+	address.sin_family = AF_INET; 
+	address.sin_addr.s_addr = INADDR_ANY; 
+	address.sin_port = htons( PORT ); 
+	
+	// Forcefully attaching socket to the port 8080 
+	if (bind(server_fd, (struct sockaddr *)&address, 
+								sizeof(address))<0) 
+	{ 
+		perror("bind failed"); 
+		exit(EXIT_FAILURE); 
+	} 
+	if (listen(server_fd, 3) < 0) 
+	{ 
+		perror("listen"); 
+		exit(EXIT_FAILURE); 
+	} 
+	if ((sock = accept(server_fd, (struct sockaddr *)&address, 
+					(socklen_t*)&addrlen))<0) 
+	{ 
+		perror("accept"); 
+		exit(EXIT_FAILURE); 
+	} 
+return 0;
+	
+} 
+
+
+
+int setup_as_client() 
+{ 
+	int valread; 
+	struct sockaddr_in serv_addr; 
+	 
+	if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) 
+	{ 
+		printf("\n Socket creation error \n"); 
+		return -1; 
+	} 
+
+	serv_addr.sin_family = AF_INET; 
+	serv_addr.sin_port = htons(PORT); 
+	
+	// Convert IPv4 and IPv6 addresses from text to binary form 
+	if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0) 
+	{ 
+		printf("\nInvalid address/ Address not supported \n"); 
+		return -1; 
+	} 
+
+	if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) 
+	{ 
+		printf("\n Setting up as server \n"); 
+		return -1; 
+	} 
+return 0;
+
+} 
+
+void SEND(){
+        send(sock , message , strlen(message) , 0 ); 
+	//printf("%s", message); 
+
+}
+void READ(){
+        int valread;
+        valread = read(sock , buffer, 1024); 
+	printf("%s\n",buffer ); 
+}
+
+int boot(){
+ int b = 0; 
+ if(setup_as_client() == -1){
+ setup_as_server();
+ b = 1;
+}
+return b;
+}
+
+
